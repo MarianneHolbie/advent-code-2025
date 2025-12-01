0e33dd00edfa9f14ae1720e069d71dc913d5d4d2
MarianneA <arrue.marianne@gmail.com>
2025-12-01 16:14:10 +0100

    Solution Day with 2 part

--- Day1/ProgramDayOne.py
+++ Day1/ProgramDayOne.py
@@ -1,3 +1,8 @@
+#####################
+#     Part One      #
+#####################
+
+
 position = 50
 pointing_zero = 0
 
@@ -24,3 +29,47 @@ with open('input.txt') as f:
         line = f.readline()
 
 print(pointing_zero)
+
+
+#####################
+#     Part Two      #
+#####################
+
+position = 50
+full_pointing_zero = 0
+
+with open('input.txt') as f:
+    line = f.readline()
+
+    while line:
+        line = line.strip()
+        if not line:
+            line = f.readline()
+            continue
+
+        number = ''.join(ch for ch in line if ch.isdigit())
+        if line[0] == 'R' :
+            new_position = (position + int(number)) % 100
+            # first click zero
+            k0 = (100 -(position % 100)) % 100
+            if k0 == 0:
+                k0 = 100
+            if k0 <= int(number):
+                count_passe = 1 + (int(number) - k0) // 100
+                full_pointing_zero += count_passe
+
+        else :
+            new_position = (position - int(number)) % 100
+
+            # first click zero
+            k0 = position % 100
+            if k0 == 0:
+                k0 = 100
+            if k0 <= int(number):
+                count_passe = 1 + (int(number) - k0) // 100
+                full_pointing_zero += count_passe
+
+        position = new_position
+        line = f.readline()
+
+print(full_pointing_zero)
